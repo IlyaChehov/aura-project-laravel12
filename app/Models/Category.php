22628075fa4d843980be71241774c4e732bc044f
Ilya Chehov <ichehoc96@gmail.com>
2025-11-25 23:48:23 +0400

    feat: implement categories system - add migration for create categories table - add Category model implement SoftDeletes, Sluggable and HasFactory traits - add CategoryFactory and CategorySeeder for create mock categories - update DatabaseSeeder for automatic create mock data

---
+++ app/Models/Category.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Category extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+    use SoftDeletes;
+    use Sluggable;
+
+    protected $table = 'categories';
+    protected $fillable = [
+        'title',
+        'slug'
+    ];
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => false,
+                'unique' => true,
+            ]
+        ];
+    }
+}
